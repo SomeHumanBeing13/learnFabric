3be6a50662de51295d1ff0834eb2d07319d4ec8f
SomeHumanBeing13 <100167628+SomeHumanBeing13@users.noreply.github.com>
2023-09-30 14:27:17 -0400

    fixed the BUAND

--- src/main/java/net/pyrotonic/learnfabric/LearnFabric.java
+++ src/main/java/net/pyrotonic/learnfabric/LearnFabric.java
@@ -2,7 +2,11 @@ package net.pyrotonic.learnfabric;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.pyrotonic.learnfabric.item.ModItems;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,10 +14,11 @@ public class LearnFabric implements ModInitializer {
 	public static final String MOD_ID = "learnFabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+public static final Item BUAND = new Item(new FabricItemSettings());
 	@Override
 	public void onInitialize() {
 
 		LOGGER.info("Hello modding world!");
-		ModItems.registerModItems();
+		Registry.register(Registries.ITEM, new Identifier("learnfabric", "buand"), BUAND);
 	}
 }
